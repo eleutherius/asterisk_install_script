35c16e521b5b711b8baa337f2d4e2ca4dd24df20
eleutherius <eleutherius69@gmail.com>
2020-03-22 14:06:57 +0200

    fix bug

--- install.sh
+++ install.sh
@@ -56,18 +56,18 @@ function configure_asterisk() {
   sudo chown -R asterisk:asterisk /var/spool/asterisk /var/run/asterisk /etc/asterisk /var/{lib,log,spool}/asterisk /usr/lib/asterisk
 
   # https://www.clearhat.org/2019/04/12/a-fix-for-apt-install-asterisk-on-ubuntu-18-04/
-  sed -i 's";\[radius\]"\[radius\]"g' /etc/asterisk/cdr.conf
-  sed -i 's";radiuscfg => /usr/local/etc/radiusclient-ng/radiusclient.conf"radiuscfg => /etc/radcli/radiusclient.conf"g' /etc/asterisk/cdr.conf
-  sed -i 's";radiuscfg => /usr/local/etc/radiusclient-ng/radiusclient.conf"radiuscfg => /etc/radcli/radiusclient.conf"g' /etc/asterisk/cel.conf
-  cp $SCRIPT_PATH/modules.conf  /etc/asterisk/modules.conf
+  sudo sed -i 's";\[radius\]"\[radius\]"g' /etc/asterisk/cdr.conf
+  sudo sed -i 's";radiuscfg => /usr/local/etc/radiusclient-ng/radiusclient.conf"radiuscfg => /etc/radcli/radiusclient.conf"g' /etc/asterisk/cdr.conf
+  sudo sed -i 's";radiuscfg => /usr/local/etc/radiusclient-ng/radiusclient.conf"radiuscfg => /etc/radcli/radiusclient.conf"g' /etc/asterisk/cel.conf
+  sudo cp $SCRIPT_PATH/modules.conf  /etc/asterisk/modules.conf
 }
 
 function add_russian_music_files() {
   wget --no-check-certificate https://github.com/pbxware/asterisk-sounds-additional/tarball/master -Oadditional-sounds.tar.gz
   sudo mkdir  /var/lib/asterisk/sounds/ru/
   sudo tar -zxvf additional-sounds.tar.gz -C /var/lib/asterisk/sounds/ru/
-  rm -f additional-sounds.tar.gz
-  sudo  mv  /var/lib/asterisk/sounds/ru/pbxware-asterisk-sounds*/*  /var/lib/asterisk/sounds/ru/
+  sudo rm -f additional-sounds.tar.gz
+  sudo mv  /var/lib/asterisk/sounds/ru/pbxware-asterisk-sounds*/*  /var/lib/asterisk/sounds/ru/
   sudo rm -rf   /var/lib/asterisk/sounds/ru/pbxware-asterisk-sounds-additional*
   sudo chown -R asterisk:asterisk /var/lib/asterisk
 
